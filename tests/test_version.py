767bb8e4dfc2ed588433d62d1b63f1c9968b0390
altescy <me@altescy.jp>
2025-10-23 00:05:43 +0900

    bump version number to v0.1.1

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,4 +6,4 @@ import romkan2
 class TestVersion(unittest.TestCase):
     def test_version(self):
         self.assertIsInstance(romkan2.__version__, str)
-        self.assertEqual(romkan2.__version__, "0.1.0")
+        self.assertEqual(romkan2.__version__, "0.1.1")
